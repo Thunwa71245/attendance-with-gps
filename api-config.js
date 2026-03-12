b9ee0ca9b60688a49716e5fdc99310efac25abad
Krit  Lokbow <kritlokbow05461@gmail.com>
2026-03-12 16:31:10 +0700

    Update check-in system features and styling
    * Added [style.css](cci:7://file:///c:/Users/Admin/Documents/GitHub/attendance-with-gps/style.css:0:0-0:0) for fully responsive and modern UI across all pages
    * Added GPS distance validation (Max 50m) to prevent remote check-ins
    * Modified check-in flow to ask for Student ID and Name before opening the camera
    * Created a custom animated modal to replace default browser success alerts

--- api-config.js
+++ api-config.js
@@ -1,2 +1,2 @@
 const GAS_API_URL =
-"https://script.google.com/macros/s/AKfycbx7LCS_ACpwPFey9dRotCaGK-NESBHqgztWIZ96ZRnNvSDgf4LXnCbpT0bx8NVE8i2B7w/exec"
+    "https://script.google.com/macros/s/AKfycbwCkJ2efxx1XnHQME0IUP195-ZUGn_hleLOp6Hj3KjwO6f2dag1Q17NVh2Mq22YlGGY/exec"
