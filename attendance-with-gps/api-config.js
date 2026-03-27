8d133bbd7842d80e6b0c1929a3dc9cc8b66a5e2c
Thanach <paithanatchaporn20@gmail.com>
2026-03-27 17:31:06 +0700

    summary

--- attendance-with-gps/api-config.js
+++ attendance-with-gps/api-config.js
@@ -1,2 +1,2 @@
 const GAS_API_URL =
-"https://script.google.com/macros/s/AKfycbyeFVJn_R3Vn480zsbxVUbyRmHhTrWG_0BOFjfxBz4/dev"
+"https://script.google.com/macros/s/AKfycbzL46O2SWPnRsOJ1b92YW096RwhfoxtBra9yDbisvXLDnxyCihH1uqMnBGMtueQpOnXBA/exec"
