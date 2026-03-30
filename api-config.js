3293e0bec0fbb0e161337e66aa719bc0b18aaec0
Dodo <santipong027@gmail.com>
2026-03-30 15:25:57 +0700

    Up12345

--- api-config.js
+++ api-config.js
@@ -1,2 +1,2 @@
 const GAS_API_URL =
-"https://script.google.com/macros/s/AKfycbyeFVJn_R3Vn480zsbxVUbyRmHhTrWG_0BOFjfxBz4/dev"
+"https://script.google.com/macros/s/AKfycbyY53fDbfWpEE1IGmm-GdVw4Je49IK-nGUhXQObYY2m/dev"
