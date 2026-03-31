58387c0c0dde1b0fcd8bfbf48f576d1f9b70341f
Dodo <santipong027@gmail.com>
2026-03-31 16:35:34 +0700

    Update ล่าสุด

--- api-config.js
+++ api-config.js
@@ -1,2 +1,2 @@
 const GAS_API_URL =
-"https://script.google.com/macros/s/AKfycbyY53fDbfWpEE1IGmm-GdVw4Je49IK-nGUhXQObYY2m/dev"
+ 'https://script.google.com/macros/s/AKfycbxkthm6Kmq61oSckBHv6wGerL2bdgvn-k1yTWbrqOjeSlcjwgV3JEjoOhq3i8933YRS1w/exec';
--- attendance-with-gps/api-config.js
+++
@@ -1,2 +0,0 @@
-const GAS_API_URL =
-"https://script.google.com/macros/s/AKfycbyeFVJn_R3Vn480zsbxVUbyRmHhTrWG_0BOFjfxBz4/dev"
